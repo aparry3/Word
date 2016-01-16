528bb44e5aad4ea1946069928f93267baea8cafc
Aaron Parry <aprry2@gmail.com>
2016-01-16 18:01:40 -0500

    Structure

--- WordControllers/WordController.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-app.controller('WordCtrl', ['$scope', 'documents', 'teachers', 'classes', 'users', 
-	function($scope, documents, teachers, classes, users) {
-		$scope.documents = documents;
-		$scope.teachers = teachers;
-		$scope.classes = classes;
-}]);
