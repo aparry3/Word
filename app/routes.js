32c5ee4d612b1f7e236b1bdc6cab1373aa233255
Aaron Parry <aprry2@gmail.com>
2016-01-17 14:11:21 -0500

    angular route

--- app/routes.js
+++ app/routes.js
@@ -1,3 +1,10 @@
 angular.module('WordApp').config(['$routeProvider', function($routeProvider) {
-	
-}]) 
+	$routeProvider
+		.when('/home', {
+			templateUrl: 'app/components/home/home.html',
+			controller: 'app/components/home/WordController.js'
+		})
+		.otherwise({
+			redirectTo:'/home'
+		})
+}]);
